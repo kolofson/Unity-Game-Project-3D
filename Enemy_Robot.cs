b9fedea51da98f3b51e447f5e081ba5644d95d09
Kevin Olofson <kolofson@nyit.edu>
2019-07-24 22:14:57 -0400

    My C# Scripts
    WIP

---
+++ Enemy_Robot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Robot : MonoBehaviour {
+
+    Rigidbody enemy_robot;
+
+    // Start is called before the first frame update
+    void Start() {
+        enemy_robot = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+
+    }
+}
---
+++ PlayerControls.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    //jump data
+    private bool grounded = false;
+    private float t = 0.0f;
+    private bool moving = false;
+    //reg setup data
+    public float speed = 1.0f / 2;
+    Rigidbody character;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        character = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown("escape")) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void FixedUpdate() {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+            character.velocity = new Vector3(0, 7, 0);
+            moving = true;
+            grounded = false;
+        }
+
+        if (moving) {
+            // when the cube has moved over 1 second report it's position
+            t = t + Time.deltaTime;
+            if (t > 1.0f) {
+                t = 0.0f;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        grounded = true;
+    }
+}
